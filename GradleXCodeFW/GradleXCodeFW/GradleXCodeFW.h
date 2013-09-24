2f60431f09bcaeda8135db41dc846684a6124813
Maciej Opala <maciek.opala@gmail.com>
2013-09-24 09:44:06 +0200

    sample framework project

---
+++ GradleXCodeFW/GradleXCodeFW/GradleXCodeFW.h
@@ -0,0 +1,13 @@
+//
+//  GradleXCodeFW.h
+//  GradleXCodeFW
+//
+//  Created by opal on 24.09.2013.
+//  Copyright (c) 2013 Apphance. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface GradleXCodeFW : NSObject
+
+@end
---
+++ GradleXCodeFW/GradleXCodeFW/GradleXCodeFW.m
@@ -0,0 +1,13 @@
+//
+//  GradleXCodeFW.m
+//  GradleXCodeFW
+//
+//  Created by opal on 24.09.2013.
+//  Copyright (c) 2013 Apphance. All rights reserved.
+//
+
+#import "GradleXCodeFW.h"
+
+@implementation GradleXCodeFW
+
+@end
